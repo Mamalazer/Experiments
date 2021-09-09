0c541189febd1e387f1ab696eabeff85831b17e5
Andrej Kalachev <AVKalachev@amdocs.com>
2021-09-09 17:20:47 +0300

    initial commit

---
+++ src/test/java/ru/bitelecom/selenide/tests/Tests.java
@@ -0,0 +1,66 @@
+package ru.bitelecom.selenide.tests;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import ru.bitelecom.selenide.pageObjects.GoogleMainPage;
+import ru.bitelecom.selenide.pageObjects.GoogleSearchResult;
+import ru.bitelecom.selenide.pageObjects.OpenMainPage;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class Tests {
+
+    @BeforeEach
+    public void option(){
+        Configuration.timeout = 6000;
+        Configuration.browser="chrome";
+        Configuration.startMaximized=true;
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-extensions");
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability(ChromeOptions.CAPABILITY, options);
+        Configuration.browserCapabilities = capabilities;
+
+    }
+
+    @Test
+    public void firstSelenide(){
+        open("https://www.google.ru/");
+        $(By.name("q")).setValue("Открытие Википедия").pressEnter();
+        ElementsCollection resultSearch = $$(By.xpath("//div[@class='g']"));
+        System.out.println(resultSearch);
+        SelenideElement elem = resultSearch.find(text("Открытие (банк) - — Википедия"));
+        System.out.println("_______");
+        System.out.println(elem.getText());
+        SelenideElement elemOtkr = $(By.xpath("//div[@class='g']")).shouldHave(text("ПАО Банк «Финансовая корпорация Открытие» (работает под брендом"));
+        elemOtkr.$(By.xpath(".//a[@href]")).click();
+        switchTo().window(1);
+        sleep(5000);
+        closeWebDriver();
+    }
+
+    @Test
+    public void otkrSelenide(){
+        GoogleMainPage googlePage = open("https://www.google.ru/", GoogleMainPage.class);
+        GoogleSearchResult googleSearchResult = googlePage.search("Открытие банк");
+        OpenMainPage openPage = googleSearchResult.goLinkByName("Банк Открытие: Частным клиентам");
+        System.out.println(openPage.getCourse("USD","Банк покупает"));
+        Assertions.assertTrue(
+                openPage.getCourse("USD","Банк покупает")
+                        <
+                        openPage.getCourse("USD","Банк продает"),
+                "Курс покупки USD не меньше курса продажи"
+        );
+    }
+
+}
---
+++ src/test/java/ru/bitelecom/selenium/Tests.java
@@ -0,0 +1,51 @@
+package ru.bitelecom.selenium;
+
+import io.qameta.allure.Description;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
+import java.util.Map;
+
+public class Tests extends WebDriverSettings {
+
+    @Test
+    public void firstTest(){
+
+    }
+
+    @Test
+    public void checkTitle(){
+        chromeDriver.get("http://www.bellintegrator.ru/");
+        String title = chromeDriver.getTitle();
+        System.out.println(title);
+        Assertions.assertTrue(title.contains("Bell Integrator"),"Тайтл не корректен");
+    }
+
+    @Test
+    public void secondTest() throws InterruptedException {
+        chromeDriver.get("https://bellintegrator.ru/index.php?route=product/search");
+        WebElement searchField = chromeDriver.findElement(By.xpath("//*[@id=\"input-search\"]"));
+        WebElement searchButton = chromeDriver.findElement(By.xpath("//*[@id=\"button-search\"]"));
+
+        searchField.click();
+        searchField.sendKeys("RPA");
+        searchButton.click();
+        Thread.sleep(5000);
+        List<WebElement> news = chromeDriver.findElements(By.xpath("//*[@class=\"product-layout product-list col-xs-12\"]//*[@class=\"short__desc\"]"));
+
+        System.out.println(news.size());
+
+        news.stream().forEach(x-> System.out.println(x.getText()));
+
+        Assertions.assertTrue(
+                news.stream().anyMatch(x->x.getText().contains("как создаются роботы"))
+                , "Заданные новости не найдены"
+        );
+    }
+
+
+}
