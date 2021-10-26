261a88a17e477970639d9ba5459f3b1d83b3724e
Daniil Kuznetsov <ldan93@yandex.ru>
2021-10-26 20:57:29 +0300

    add basicAuth, checkBoxes, contextMenu tests

--- src/test/java/ru/bitelecom/selenium/internet/Tests.java
+++ src/test/java/ru/bitelecom/selenium/internet/Tests.java
@@ -5,11 +5,12 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import java.util.List;
 
-import static ru.bitelecom.selenium.internet.Urls.addRemoveElements;
+import static ru.bitelecom.selenium.internet.Urls.*;
 
 public class Tests extends WebDriverSettings {
 
@@ -21,6 +22,48 @@ public class Tests extends WebDriverSettings {
         wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[text()='Delete']"))).click();
         List<WebElement> deleteButton = chromeDriver.findElements(By.xpath("//button[text()='Delete']"));
         Assertions.assertEquals(0, deleteButton.size(), "Кнопка Delete осталась на странице");
+    }
+
+    @Test
+    @DisplayName("Basic authorisation")
+    public void basicAuth() {
+        chromeDriver.get(basicAuth.value);
+        String text = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='example']/p"))).getText();
+        Assertions.assertEquals(text, "Congratulations! You must have the proper credentials.", "Ошибка при авторизации");
+    }
+
+    @Test
+    @DisplayName("Checkboxes")
+    public void checkBoxes() {
+        chromeDriver.get(checkBoxes.value);
+        WebElement firstCheckBox = chromeDriver.findElement(By.xpath("//br/preceding-sibling::input"));
+        WebElement secondCheckBox = chromeDriver.findElement(By.xpath("//br/following-sibling::input"));
+
+        if (!firstCheckBox.isSelected()) {
+            firstCheckBox.click();
+            Assertions.assertTrue(firstCheckBox.isSelected(), "Чек бокс не активирован");
+        }
+
+        if (secondCheckBox.isSelected()) {
+            secondCheckBox.click();
+            Assertions.assertFalse(secondCheckBox.isSelected(), "Чек бокс не дезактивирован");
+        }
+    }
+
+    @Test
+    @DisplayName("Context menu")
+    public void contextMenu() {
+        chromeDriver.get(contextMenu.value);
+        WebElement button = chromeDriver.findElement(By.xpath("//div[@id='hot-spot']"));
+        Actions action = new Actions(chromeDriver);
+        action.contextClick(button).build().perform();
+        String alert = wait.until(ExpectedConditions.alertIsPresent()).getText();
+        if (alert.equals("You selected a context menu")) {
+            chromeDriver.switchTo().alert().accept();
+        } else {
+            Assertions.fail("Правая кнопка мыши не сработала");
+        }
+
     }
 
 }
--- src/test/java/ru/bitelecom/selenium/internet/Urls.java
+++ src/test/java/ru/bitelecom/selenium/internet/Urls.java
@@ -2,7 +2,10 @@ package ru.bitelecom.selenium.internet;
 
 public enum Urls {
 
-    addRemoveElements("http://the-internet.herokuapp.com/add_remove_elements/");
+    addRemoveElements("http://the-internet.herokuapp.com/add_remove_elements/"),
+    basicAuth("http://admin:admin@the-internet.herokuapp.com/basic_auth"),
+    checkBoxes("http://the-internet.herokuapp.com/checkboxes"),
+    contextMenu("http://the-internet.herokuapp.com/context_menu");
 
     Urls(String url) {
         this.value = url;
