6e383c8d1e7a8a52c433ae030e39eba508c3e4f4
Daniil Kuznetsov <ldan93@yandex.ru>
2021-11-21 13:41:05 +0300

    update

--- src/test/java/ru/bitelecom/selenium/WebDriverSettings.java
+++ src/test/java/ru/bitelecom/selenium/WebDriverSettings.java
@@ -16,8 +16,8 @@ public class WebDriverSettings {
 
     @BeforeEach
     public void installSettings() {
-        //System.setProperty("webdriver.chrome.driver","src/main/resources/chromedriver.exe");
-        System.setProperty("webdriver.chrome.driver", System.getenv("CHROME_DRIVER"));
+        System.setProperty("webdriver.chrome.driver","src/main/resources/chromedriver.exe");
+        //System.setProperty("webdriver.chrome.driver", System.getenv("CHROME_DRIVER"));
         chromeDriver = new ChromeDriver();
         chromeDriver.manage().window().maximize();
         chromeDriver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
