8584b858f7127bf8a5983bb19f0b1a2e85858aaf
Daniil Kuznetsov <ldan93@yandex.ru>
2021-11-30 21:14:54 +0300

    add new test and fix old tests into selenide.internet

--- src/test/java/ru/bitelecom/selenide/internet/Tests.java
+++ src/test/java/ru/bitelecom/selenide/internet/Tests.java
@@ -9,6 +9,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+
 import static com.codeborne.selenide.Selenide.*;
 import static ru.bitelecom.selenide.internet.Urls.*;
 
@@ -16,10 +19,13 @@ public class Tests {
 
     @BeforeEach
     public void option() {
-        Configuration.timeout = 6000; // установка задержки в милисекундах
-//        Configuration.browser = "chrome";
-        Configuration.browser = "firefox";
+        Configuration.timeout = 10000; // установка задержки в милисекундах
+        Configuration.browser = "chrome";
+//        Configuration.browser = "firefox";
         Configuration.startMaximized = true;
+        Configuration.fastSetValue = true; //оптимизация мeтода setValue(), если перестанет работать, то можно
+                                           // использовать sendKeys()
+//        Configuration.headless = false; // браузер не будет отображаться при запуске тестов
 
         // Работает только в версии 5.5.0, не выше
         ChromeOptions chromeOptions = new ChromeOptions();
@@ -117,12 +123,13 @@ public class Tests {
     @DisplayName("Drag and Drop")
     public void dragAndDrop() {
 
+        open(dragAndDrop.url);
         SelenideElement a = $x("//p[text()='Drag me to my target']/parent::div");
         SelenideElement b = $x("//p[text()='Drop here']/parent::div");
         SelenideElement check = $x("//div[@id='droppable']/p");
-        open(dragAndDrop.url);
 
-        a.dragAndDropTo(b);
+        actions().moveToElement(a).build().perform();
+        actions().clickAndHold().moveToElement(b).release().build().perform();
         Assertions.assertEquals("Dropped!", check.getText(), "Элемент не перенесён");
     }
 
@@ -172,4 +179,112 @@ public class Tests {
         textField.shouldBe(Condition.disabled);
     }
 
+    @Test
+    @Owner("DRKuznetsov")
+    @Tag("Internet")
+    @DisplayName("Entry Ad(modal window)")
+    public void entryAd() {
+        open(entryAd.url);
+        $x("//p[text()='Close']").shouldBe(Condition.visible).click();
+        $x("//a[@id='restart-ad']").shouldBe(Condition.visible).click();
+        $x("//p[text()='Close']").shouldBe(Condition.visible).click();
+    }
+
+    @Test
+    @Owner("DRKuznetsov")
+    @Tag("Internet")
+    @DisplayName("Download file")
+    public void downloadFile() throws FileNotFoundException {
+        open(downloadFile.url);
+        $x("//a[contains(text(), 'test')]").download();
+    }
+
+    @Test
+    @Owner("DRKuznetsov")
+    @Tag("Internet")
+    @DisplayName("Upload file")
+    public void uploadFile() {
+        open(uploadFile.url);
+        File file = new File("C:\\Users\\drkuznetsov\\IdeaProjects\\bi_telecom\\Test.txt");
+        $x("//input[@id='file-upload']").uploadFile(file);
+        $x("//input[@type='submit']").click();
+        $x("//h3[text()='File Uploaded!']").shouldBe(Condition.visible);
+    }
+
+    @Test
+    @Owner("DRKuznetsov")
+    @Tag("Internet")
+    @DisplayName("Login Page positive")
+    public void formAuthenticationPos() {
+
+        open(formAuthentication.url);
+        $x("//input[@name='username']").setValue("tomsmith");
+        $x("//input[@name='password']").setValue("SuperSecretPassword!");
+        $x("//button[@type='submit']").click();
+        $x("//a[@class='button secondary radius']").shouldBe(Condition.visible).click();
+        $x("//h2[text()='Login Page']").shouldHave(Condition.text("Login Page"));
+    }
+
+    @Test
+    @Owner("DRKuznetsov")
+    @Tag("Internet")
+    @DisplayName("Login Page negative")
+    public void formAuthenticationNeg() {
+
+        open(formAuthentication.url);
+        $x("//input[@name='username']").setValue("tomsmith");
+        $x("//input[@name='password']").setValue("tomsmith");
+        $x("//button[@type='submit']").click();
+        String error = $x("//div[@id='flash']").getText();
+        Assertions.assertTrue(error.contains("Your password is invalid!"),
+                "Ошибка при авторизации не отображается");
+    }
+
+    @Test
+    @Owner("DRKuznetsov")
+    @Tag("Internet")
+    @DisplayName("Nested Frames")
+    public void nestedFrames() {
+        open(frames.url);
+        $x("//a[text()='Nested Frames']").click();
+        switchTo().frame("frame-top");
+
+        String leftFrame = switchTo().frame("frame-left").findElement(By.xpath("//body")).getText();
+        Assertions.assertEquals(leftFrame, "LEFT", "Ошибка при переключении на левый frame");
+
+        switchTo().parentFrame();
+
+        String middleFrame = switchTo().frame("frame-middle").findElement(By.xpath("//body")).getText();
+        Assertions.assertEquals(middleFrame, "MIDDLE", "Ошибка при переключении на средний frame");
+
+        switchTo().parentFrame();
+
+        String rightFrame = switchTo().frame("frame-right").findElement(By.xpath("//body")).getText();
+        Assertions.assertEquals(rightFrame, "RIGHT", "Ошибка при переключении на правый frame");
+
+        switchTo().window(0);
+
+        String bottomFrame = switchTo().frame("frame-bottom").findElement(By.xpath("//body")).getText();
+        Assertions.assertEquals(bottomFrame, "BOTTOM", "Ошибка при переключении на bottom frame");
+
+    }
+
+    @Test
+    @Owner("DRKuznetsov")
+    @Tag("Internet")
+    @DisplayName("iFrame")
+    public void iFrame() {
+        open(frames.url);
+        SelenideElement textField = $x("//body[@id='tinymce']/p");
+        $x("//a[text()='iFrame']").click();
+        switchTo().frame("mce_0_ifr");
+        textField.clear();
+        textField.sendKeys("Hello!");
+        Assertions.assertEquals(textField.getText(), "Hello!", "Текст не отображается в текстовом поле");
+
+        switchTo().window(0);
+
+        $x("//button[@aria-label='Align center']").click();
+
+    }
 }
